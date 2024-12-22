05725f7bec44fa72cb057558cea38af218ac8cfd
Linus <linus.hasselkvist@gmail.com>
2024-12-22 16:17:57 +0100

    init command update - copy template to project

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
+	"runtime"
 )
+
+// type Project interface {
+// }
 
 func main() {
 	if len(os.Args) < 2 {
@@ -28,18 +34,77 @@ func main() {
 
 func initCommand() {
 	if len(os.Args) == 2 {
-		// create blank project
-		fmt.Printf("Succesfully initialized emty project in current directory.\n")
+		fmt.Printf("Succesfully initialized linxr project in current directory.\n")
 	} else if len(os.Args) == 3 {
 		if os.Args[2] == "blank" {
-			// create blank project
-			fmt.Printf("Succesfully initialized emty project in current directory.\n")
-		} else if os.Args[2] == "SDL2_C" {
-			// create SDL2 project
-			fmt.Printf("Succesfully initialized project using the SDL2_C template in current directory.\n")
+			fmt.Printf("Succesfully initialized linxr project in current directory.\n")
+		} else {
+			fmt.Printf("Trying to copy template to current directory...\n")
+			templateDir := filepath.Join(getTemplateDir(), os.Args[2])
+			err := copyTemplate(templateDir, ".")
+			if err != nil {
+				fmt.Printf("Error initializing project: %v\n", err)
+			} else {
+				fmt.Println("Project files copied successfully.")
+				fmt.Printf("Succesfully initialized linxr project using the SDL2_C template in current directory.\n")
+			}
+		}
+	}
+}
+
+func getTemplateDir() string {
+	if runtime.GOOS == "windows" {
+		appData := os.Getenv("APPDATA")
+		return filepath.Join(appData, "linxr", "templates")
+	} else {
+		homeDir, err := os.UserHomeDir()
+		if err != nil {
+			panic("Could not determine home directory")
+		}
+		return filepath.Join(homeDir, ".linxr", "templates")
+	}
+}
+
+func copyTemplate(template, dest string) error {
+	return filepath.Walk(template, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(template, path)
+		if err != nil {
+			return err
+		}
+		targetPath := filepath.Join(dest, relPath)
+		if info.IsDir() {
+			return os.MkdirAll(targetPath, info.Mode())
 		}
 
+		return copyFile(path, targetPath)
+	})
+}
+
+func copyFile(templateFile, destFile string) error {
+	src, err := os.Open(templateFile)
+	if err != nil {
+		return err
 	}
+	defer src.Close()
+
+	dst, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+	templateInfo, err := os.Stat(templateFile)
+	if err != nil {
+		return err
+	}
+	return os.Chmod(destFile, templateInfo.Mode())
 }
 
 func helpCommand() {
